731692a46bf3782edd225bba87ce98ee6ac7d46d
Constant10fps no-name <konstantin.s.kozlov@gmail.com>
2024-04-25 14:36:22 +0000

    created project factory

---
+++ src/lib/components/projectCard.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let url;
+    export let title;
+    export let image;
+    export let description;
+</script>
+
+<a
+    href={url}
+    class="border border-white/30 bg-slate-800 rounded-xl p-5 space-y-1"
+>
+    <img src={image} alt="logo" class="size-36 mb-5" />
+    <div class="text-xl font-bold">{title}</div>
+    <div class="text-lg">
+        {description}
+    </div>
+</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-	import Shooter from "$lib/components/projects/shooter.svelte";
-	import Website from "$lib/components/projects/website.svelte";
+	import ProjectCard from "$lib/components/projectCard.svelte";
 	import { Github, Twitter, Send, Slack } from "lucide-svelte";
 	export function copyDiscordId() {
 		navigator.clipboard.writeText("сonstant0fps");
@@ -10,9 +9,9 @@
 <!--@me and contacts menu-->
 <div class="flex flex-col items-center gap-3 py-12">
 	<img src="/favicon.png" alt="avatar" class="rounded-full aspect-square" />
-	<div class="text-2xl font-extrabold">Constant0fps</div>
-	<div class="align-center text-center">
-		Pythonista, 2 years of experience/ TS | JS junior-intermediate level
+	<div class="text-3xl font-extrabold">Constant0fps</div>
+	<div class="align-center text-center text-lg">
+		Pythonista, 2 years of experience <br /> TS・JS junior-intermediate level
 	</div>
 	<div class="flex gap-3">
 		<a
@@ -23,10 +22,10 @@
 			class="p-3 rounded-xl bg-slate-800 hover:bg-slate-700 active:scale-90 transition-all"
 			href="https://github.com/Constant10fps"><Github /></a
 		>
-		<button
+		<!--<button
 			class="p-3 rounded-xl bg-slate-800 hover:bg-slate-700 active:scale-90 transition-all"
 			on:click={copyDiscordId}><Slack /></button
-		>
+		>-->
 		<a
 			class="p-3 rounded-xl bg-slate-800 hover:bg-slate-700 active:scale-90 transition-all"
 			href="https://twitter.com/Konstan91655710"><Twitter /></a
@@ -34,7 +33,17 @@
 	</div>
 </div>
 <!--Projects-->
-<div class="grid lg:grid-cols-2 gap-3">
-	<Shooter />
-	<Website />
+<div class="grid md:grid-cols-2 xl:grid-cols-4 gap-3">
+	<ProjectCard
+		title="Python Project Example"
+		image="/pythonlogo.png"
+		url="https://github.com/Constant10fps/shooter"
+		description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla diam enim, euismod ultricies volutpat in. "
+	/>
+	<ProjectCard
+		title="TS Project Example"
+		image="/tslogo.png"
+		url="https://github.com/Constant10fps/site"
+		description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla diam enim, euismod ultricies volutpat in. "
+	/>
 </div>
