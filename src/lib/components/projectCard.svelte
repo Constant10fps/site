7da201d3386035ec091e947f5de6cdb506f2d70b
Constant10fps no-name <konstantin.s.kozlov@gmail.com>
2024-10-13 10:01:02 +0000

    projectcard image takes up all space it has + site renovations

--- src/lib/components/projectCard.svelte
+++ src/lib/components/projectCard.svelte
@@ -7,9 +7,10 @@
 
 <a
     href={url}
+    target="_blank"
     class="border border-white/30 bg-slate-800 rounded-xl p-5 space-y-1"
 >
-    <img src={image} alt="logo" class="size-36 mb-5" />
+    <img src={image} alt="logo" class="mb-5 brightness-150" />
     <div class="text-xl font-bold">{title}</div>
     <div class="text-lg">
         {description}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,14 +12,16 @@
     <img src="/favicon.png" alt="avatar" class="rounded-full aspect-square" />
     <div class="text-3xl font-extrabold">Constant0fps</div>
     <div class="align-center text-center text-lg">
-        Pythonista, 2 years of experience <br /> TS・JS junior-intermediate level
+        TS・JS junior-intermediate level <br> C++ enjoyer
     </div>
     <div class="flex gap-3">
         <a
+            target="_blank"
             class="p-3 rounded-xl bg-slate-800 hover:bg-slate-700 active:scale-90 transition-all"
             href="https://t.me/Constant0fps"><Send /></a
         >
         <a
+            target="_blank"
             class="p-3 rounded-xl bg-slate-800 hover:bg-slate-700 active:scale-90 transition-all"
             href="https://github.com/Constant10fps"><Github /></a
         >
@@ -28,6 +30,7 @@
             on:click={copyDiscordId}><Discord /></button
         >
         <a
+            target="_blank"
             class="p-3 rounded-xl bg-slate-800 hover:bg-slate-700 active:scale-90 transition-all"
             href="https://twitter.com/Konstan91655710"><Twitter /></a
         >
@@ -36,15 +39,15 @@
 <!--Projects-->
 <div class="grid md:grid-cols-2 xl:grid-cols-4 gap-3">
     <ProjectCard
-        title="Python Project Example"
-        image="/pythonlogo.png"
-        url="https://github.com/Constant10fps/shooter"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla diam enim, euismod ultricies volutpat in. "
+        title="Portfolio Website"
+        image="/site.png"
+        url="https://github.com/Constant10fps/site"
+        description="The website you're currently on. In development"
     />
     <ProjectCard
-        title="TS Project Example"
-        image="/tslogo.png"
+        title="test"
+        image="/site.png"
         url="https://github.com/Constant10fps/site"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla diam enim, euismod ultricies volutpat in. "
+        description="test"
     />
 </div>
