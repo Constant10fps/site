c63cddec6b46b1656ea5ae8e7ff5c9526910fd2c
Constant10fps <konstantin.s.kozlov@gmail.com>
2024-12-21 23:21:41 +0500

    Change Card component

--- components/Card.tsx
+++ components/Card.tsx
@@ -5,7 +5,7 @@ export default function Card(
     <main>
       <button class="container p-5 rounded-2xl bg-neutral-700 border border-neutral-400/30 hover:bg-neutral-600">
         <a href={page_url}>
-          <div class="m-2 text-xl font-bold">{title}</div>
+          <div class="text-xl font-bold">{title}</div>
           <div>{desc}</div>
         </a>
       </button>
